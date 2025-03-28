6a85256dbc3f2e4821852d686410cb5fdbecd202
fasilofficial <muhammedfasilofficial@gmail.com>
2025-03-28 20:13:24 +0530

    Update webhook url

--- crest/settings.php
+++ crest/settings.php
@@ -1,5 +1,5 @@
 <?php
-define('C_REST_WEB_HOOK_URL', 'https://mondus.group/rest/1/c3iwswykiqvhzc71/'); //url on creat Webhook
+define('C_REST_WEB_HOOK_URL', 'https://mondus.group/rest/1/dw9gd4xauhctd7ha/'); //url on creat Webhook
 define('C_REST_CLIENT_ID', 'local.67e1ed8665f771.26018866');
 define('C_REST_CLIENT_SECRET', 'bMtzdhgaYmSFjSaF4I1JtxRaoRZ5hxfB9K6duV2PeJ9ubu9SgB');
 define('C_REST_BLOCK_LOG', true);
